e6bdcd766bdfeb4c7f2c7bd13699839b3155d028
srilman <slade@wpi.edu>
2017-05-21 05:07:47 -0400

    COrs

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 var googleMapsClient = require('@google/maps').createClient({
   key: "AIzaSyDYZFkraP5rrFg_7LI6erhExUkwlulIAqA"
 });
